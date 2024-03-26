ff0c5b1d3de76d07635e9cab821b2b46cbc42184
MalumaDev <piano.lu92@gmail.com>
2024-03-26 15:28:34 +0100

    add ltn

---
+++ ltn/__init__.py
@@ -0,0 +1,7 @@
+from ltn import fuzzy_ops
+from ltn import utils
+from ltn.core import (Predicate, Function, Variable, Constant, Proposition, diag, undiag,
+        diag_lock, as_tensors, Wrapper_Connective, Wrapper_Quantifier, Wrapper_Formula_Aggregator,
+        Formula)
+from ltn import wrapper
+from ltn import log
